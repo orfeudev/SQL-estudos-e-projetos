ca6f11a6e9202648116cf669d7943e339be1edab
orfeudev <francgbsj1995@gmail.com>
2025-03-15 11:39:17 -0300

    finalizando joins

--- 7.3 Joins + group by + order by - northwind.sql
+++ 7.3 Joins + group by + order by - northwind.sql
@@ -16,5 +16,28 @@ LEFT JOIN products
 ON order_details.product_id = products.product_id
 GROUP BY product_name
 ORDER BY quantidade_total DESC;
-	
 
+-- exemplo 2: Ainda utilizando o código da análise anterior, faça o agrupamento de quantidade_total
+-- por product_name considerando apenas os produtos da classe luxo(preço acima de R$ 80)
+
+SELECT
+	product_name,
+	SUM(quantity) AS quantidade_total
+FROM order_details
+LEFT JOIN products 
+ON order_details.product_id = products.product_id
+WHERE products.unit_price >= 80
+GROUP BY product_name
+ORDER BY quantidade_total DESC;
+
+-- consider agora a quantidade_total >= 1000
+
+SELECT
+	product_name,
+	SUM(quantity) AS quantidade_total
+FROM order_details
+LEFT JOIN products 
+ON order_details.product_id = products.product_id
+GROUP BY product_name
+HAVING SUM(quantity) >= 1000
+ORDER BY quantidade_total DESC;
