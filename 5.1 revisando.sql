f35fe6d92274c38b3ef47727470d21b93e4ba321
orfeudev <francgbsj1995@gmail.com>
2025-03-16 23:28:37 -0300

    revisao

---
+++ 5.1 revisando.sql
@@ -0,0 +1,21 @@
+-- revisando conceitos
+
+SELECT * FROM order_details -- O select serve para iniciar uma consulta
+WHERE quantity >= 10 -- o WHERE é um filtro
+ORDER BY quantity DESC; -- ORDER BY é um ordenador da tabela que aceita condições
+
+SELECT COUNT(DISTINCT quantity) FROM order_details; -- conta o numero de linhas de valores distintos em uma coluna
+
+SELECT
+	SUM(quantity) as "quantidade"
+FROM order_details;
+
+--
+
+SELECT 
+	contact_title,
+	COUNT(1)
+FROM customers -- seleciona a coluna contact_title de customers e faz uma contagem de pessoas por titulos
+GROUP BY contact_title -- agrupa por titulo
+ORDER BY COUNT(1) DESC; -- ordena a contagem de forma decrescente.
+
